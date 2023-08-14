6fc59b50bb5c3b5f1f63f963292c98d8994ef2c8
BijianWu <384318185@qq.com>
2023-08-14 23:06:01 +0100

    added in all old styles

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 
 import "./assets/css/styles.css"
+import Nav from "./components/Nav";
 export default function App(){
     return <>
+        <Nav />
         <h1>test app</h1>
     </>
 }
---
+++ src/components/Nav.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+export default function Nav(){
+    return <>
+        <h1>my nav bar</h1>
+    </>
+}
