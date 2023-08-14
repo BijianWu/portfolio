78f6566bb85c4a2a2f63fed6d182ef92760444bd
BijianWu <384318185@qq.com>
2023-08-14 23:29:46 +0100

    adding home page

--- src/App.js
+++ src/App.js
@@ -3,9 +3,11 @@ import React from "react";
 import "./assets/css/styles.css"
 import Nav from "./components/Nav";
 import Footer from "./components/Footer";
+import Home from "./components/Home";
 export default function App(){
     return <>
         <Nav />
+        <Home />
         <Footer />
     </>
 }
