b1e390f0fcf4c2a37e089c875f92fd6402be6c4f
BijianWu <384318185@qq.com>
2023-08-16 22:30:25 +0100

    togle nav buttons works

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,12 +1,30 @@
-import React from "react";
+import React, { useRef } from "react";
 
 export default function Nav(){
+    const themeButton = useRef();
+    const navMenu = useRef();
+    const darkTheme = "dark-theme";
+    const iconTheme = "uil-sun";    
+    const toggleTheme = () => {
+        document.body.classList.toggle(darkTheme);
+        themeButton.current.classList.toggle(iconTheme);
+    }
+
+    const toggleMenu = () => {
+        console.log("toggleMenu clicked")
+        if (!navMenu.current.classList.contains("show-menu")) {
+            navMenu.current.classList.add("show-menu");
+        } else {    
+            navMenu.current.classList.remove("show-menu");
+        }
+
+    }
     return <>
         <header className="header" id="header">
         <nav className="nav container">
             <a href="#" className="nav__logo">Bijian</a>
 
-            <div className="nav__menu" id="nav-menu">
+            <div className="nav__menu" id="nav-menu" ref={navMenu}>
                 <ul className="nav__list grid">
                     <li className="nav__item">
                     <a href="#home" className="nav__link active-link">
@@ -39,13 +57,13 @@ export default function Nav(){
                     </a>
                     </li>
                 </ul>
-                <i className="uil uil-times nav__close" id="nav-close"></i>
+                <i className="uil uil-times nav__close" id="nav-close" onClick={() =>toggleMenu()}></i>
             </div>
 
             <div className="nav__btns">
-                <i className="uil uil-moon change-theme" id="theme-button"></i>
+                <i className="uil uil-moon change-theme" id="theme-button" ref={themeButton} onClick={() =>toggleTheme()} ></i>
 
-                <div className="nav__toggle" id="nav-toggle">
+                <div className="nav__toggle" id="nav-toggle" onClick={() =>toggleMenu()}>
                     <i className="uil uil-apps"></i>
                 </div>
             </div>
