ee978ca5c015737170d62312b858b0be65ac749d
BijianWu <384318185@qq.com>
2023-08-17 00:14:09 +0100

    show second app

--- src/components/AppsWork.js
+++ src/components/AppsWork.js
@@ -4,21 +4,24 @@ export default function AppsWork(){
   const[modalOn_1, setModalOn_1] = useState(false);
   const modalStyles_1 = modalOn_1 ? "active-modal" : "";
 
+  const[modalOn_2, setModalOn_2] = useState(false);
+  const modalStyles_2 = modalOn_2 ? "active-modal" : "";
+
     return <>
 
       <section className="services section" id="services">
         <h2 className="section__title">Apps</h2>
         <span className="section__subtitle">What i have created</span>
 
-        <div className="services__container container">
+        <div className="services__container container myapps__img_container">
           {/* <!--====== SERVICES 1=======--> */}
           <div className="services__content">
-            <div className=" myapps__img_container">
+            <div className=" ">
 
               <h3 className="services__title">
               <span className="uil uil-web-grid services__icon"></span>
                 ForeverG -
-                <i className="testimonial__client">Fullstack web app</i>
+                <i className="testimonial__client">Fullstack</i>
                 
               </h3>
               <div className="">
@@ -33,9 +36,41 @@ export default function AppsWork(){
                 <i className="uil uil-arrow-right button__icon"></i>
               </span>
             </div>
+          </div>
 
 
-            <div className={`services__modal ${modalStyles_1}`}>
+
+          {/* <!--====== SERVICES 2=======--> */}
+          <div className="services__content">
+            <div className=" ">
+
+              <h3 className="services__title">
+              <span className="uil uil-web-grid services__icon"></span>
+                Portfolio  - 
+                <i className="testimonial__client">Frontend</i>
+                
+              </h3>
+              <div className="">
+                <img src="assets/images/porfolio-web.png" alt="" className="myapps__img" />
+              </div>
+
+                <span
+                className="button button--flex button--small button--link services__button"
+                onClick={() =>setModalOn_2(true)}
+              >
+                View More
+                <i className="uil uil-arrow-right button__icon"></i>
+              </span>
+            </div>
+          </div>
+          
+
+
+        </div>
+
+{/* belows are for modals */}
+                    {/* modal for app 1 */}
+                    <div className={`services__modal ${modalStyles_1}`}>
               <div className="services__modal-content">
                 <h4 className="services__modal-title">ForeverG</h4>
                 <i className="uil uil-times services__modal-close" onClick={() =>setModalOn_1(false)}></i>
@@ -68,9 +103,34 @@ export default function AppsWork(){
                 </a>
               </div>
             </div>
+          
+            {/* modal for app 2 */}
+            <div className={`services__modal ${modalStyles_2}`}>
+              <div className="services__modal-content">
+                <h4 className="services__modal-title">Personal Porfolio</h4>
+                <i className="uil uil-times services__modal-close" onClick={() =>setModalOn_2(false)}></i>
+                <p className="services__modal-des">
+                  Made by using ReactJS, Html and plain css, which has features like
+                </p>
+                <ul className="services__modal-services grid">
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p>Light and dark theme switches</p>
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p>Responsive layouts</p>
+                  </li>
+                  <li className="services__modal-service">
+                    <i className="uil uil-check-circle services__modal-icon"></i>
+                    <p>Full fleged navigation menu</p>
+                  </li>
+                </ul>
+                <br />
+ 
+              </div>
+            </div>
 
-          </div>
-        </div>
       </section>
     </>
 }
