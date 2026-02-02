907af1af6d32eda41fb05c83c708d480726b08c6
BijianWu <384318185@qq.com>
2026-02-02 21:26:38 +0000

    change to 6+ years of experience

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -24,7 +24,7 @@ export default function AboutMe(){
 
             <div className="about__info">
               <div>
-                <span className="about__info-title">05+</span>
+                <span className="about__info-title">06+</span>
                 <span className="about__info-name"
                   >Years <br />
                   experience</span
